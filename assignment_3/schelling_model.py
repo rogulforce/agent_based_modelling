f9eaeaab49bd1c4e251eb1e1e1c8788893c639b0
Piotr Rogula <piotr.r99@gmail.com>
2022-10-27 16:22:10 +0200

    working on list 3

--- assignment_3/schelling_model.py
+++ assignment_3/schelling_model.py
@@ -3,11 +3,15 @@ import itertools
 import numpy as np
 # from assignment_3.utils import to_list, from_list
 # from .utils import to_list, from_list
-def from_list(list_of_cords: list[list[int, int]]):
+import random
+from copy import copy
+
+
+def from_list(list_of_cords):
     return [[it[0] for it in list_of_cords], [it[1] for it in list_of_cords]]
 
 
-def to_list(cords: list[list[int], list[int]]):
+def to_list(cords):
     return [it for it in zip(cords[0], cords[1])]
 
 # 1 iteration - get all unhappy agents, move it one by one
@@ -33,6 +37,9 @@ class SchellingModel:
         self.lattice = self._generate_lattice()
         # self.m = self._get_m_parameter()
         self.empty_spots = []
+        self.agents = []
+
+        self.run = True
 
     # def _get_m_parameter(self):
     #     """ Get m (num of neighbours) parameter according to neighbourhood range. """
@@ -47,8 +54,12 @@ class SchellingModel:
         return self.lattice
 
     def _update_empty_spots(self):
-        self.empty_spots = np.where(self.lattice == 0)
+        self.empty_spots = np.argwhere(self.lattice == 0).tolist()
         return self.empty_spots
+
+    def _update_agents(self):
+        self.agents = np.argwhere(self.lattice != 0).tolist()
+        return self.agents
 
     def get_neighbours(self, agent_index: tuple[int, int]):
         """ Get all the neighbours of given agent_index (row_num, col_num)
@@ -56,10 +67,10 @@ class SchellingModel:
             agent_index: given agent_index
         Returns:
             neighbour list. """
-        nb_row = [it for it in range(agent_index[0] - self.neighbourhood_range, agent_index[0] + self.neighbourhood_range + 1)
+        nb_row = [it for it in range(agent_index[0] - self.neighbourhood_range, (agent_index[0] + self.neighbourhood_range + 1) % self.size)
                   # if 0 <= it < self.size
                   ]
-        nb_col = [it for it in range(agent_index[1] - self.neighbourhood_range, agent_index[1] + self.neighbourhood_range + 1)
+        nb_col = [it for it in range(agent_index[1] - self.neighbourhood_range, (agent_index[1] + self.neighbourhood_range + 1) % self.size)
                   # if 0 <= it < self.size
                   ]
         neighbours = itertools.product(nb_row, nb_col)
@@ -72,24 +83,67 @@ class SchellingModel:
         occupied_cells = np.sum(self.lattice[neighbours[0], neighbours[1]] != 0)
         similar_cells = np.sum(self.lattice[neighbours[0], neighbours[1]] == agent_type)
 
+        if occupied_cells == 0:
+            return 1
+
         return similar_cells / occupied_cells
 
-    def validate_happiness(self, agent_index: tuple[int, int]):
-        agent_type = self.lattice[agent_index[0], agent_index[1]]
+    def _get_type(self, agent_index):
+        return self.lattice[agent_index[0], agent_index[1]]
+
+    def validate_happiness(self, agent_index: tuple[int, int], agent_type: int):
         return self.get_happiness(agent_index, agent_type) >= self.j_t[int(agent_type)-1]
 
+    def play(self, iter_num):
+
+        for i in range(iter_num):
+            self.single_iteration()
+            if not self.run:
+                print(f'{i} iterations')
+                break
+        print(self.lattice)
+
+    def single_iteration(self):
+        self.run = False
+        self._update_empty_spots()
+        list_of_agents = copy(self._update_agents())
+
+        # shuffle
+        random.shuffle(list_of_agents)
+
+        for agent in list_of_agents:
+            agent_type = self._get_type(agent)
+            # check for empty spots
+            print(agent, self.validate_happiness(agent, agent_type))
+            if not self.validate_happiness(agent, agent_type):  # agent is unhappy
+                self.move(agent, agent_type)
+                self.run = True
+
+    def move(self, agent, agent_type):
+        new_place = random.choice(self.empty_spots)
+
+        self.lattice[agent[0], agent[1]] = 0
+        self.lattice[new_place[0], new_place[1]] = agent_type
+
+        self.agents.remove(agent)
+        self.empty_spots.append(agent)
+
+        self.agents.append(new_place)
+        self.empty_spots.remove(new_place)
 
 
 if __name__ == "__main__":
-    a = SchellingModel(size=10, neighbourhood_range=1, n_agents=(50, 30))
-
+    a = SchellingModel(size=10, neighbourhood_range=1, n_agents=(15, 15), j_t=(0.5, 0.5))
+    # print(a.lattice)
     # print(a.get_neighbours((5,8)))
     # print(a._get_m_parameter())
     # print(a.lattice)
     # print())
     # n = from_list(a.get_neighbours((0, 0)))
     # a.lattice[n[0], n[1]] = 8
-    index = (2,2)
+
+    index = (2, 2)
     print(a.lattice)
     print(a.validate_happiness(index))
-
+    # a.play(100)
+    # TODO: test that in jupyter
